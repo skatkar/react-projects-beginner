3643eddba510420bd3d0c420959ae7114c945f19
Swapnil Katkar <skatkar225@gmail.com>
2023-10-18 19:45:55 +0530

    Update ThemeChange.js

--- src/components/ThemeChange/ThemeChange.js
+++ src/components/ThemeChange/ThemeChange.js
@@ -1,1 +1,32 @@
+import classes from "./ThemeChange.module.css";
+import ThemeContext from "../../store/theme-context";
+import { useContext } from "react";
+
+const ThemeChange = () => {
+  const { themes, selectedTheme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <>
+      <h1>Project 8: Theme change with context</h1>
+      <div className={classes.container}>
+        <div className={classes.row}>
+          <h2>Theme</h2>
+          <label class={classes.switch}>
+            <input type="checkbox" onChange={toggleTheme} />
+            <span className={`${classes.slider} ${classes.round}`}></span>
+          </label>
+        </div>
+        <div
+          style={{
+            background: themes[selectedTheme].background,
+            color: themes[selectedTheme].text,
+          }}
+        >
+          This component adopts the theme change
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ThemeChange;
 
