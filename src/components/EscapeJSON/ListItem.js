f06459b63013658c2502a455430a8cfb8ec63180
Swapnil Katkar <skatkar225@gmail.com>
2023-12-01 15:44:51 -0600

    CSS modification

--- src/components/EscapeJSON/ListItem.js
+++ src/components/EscapeJSON/ListItem.js
@@ -1,7 +1,11 @@
 import classes from "./ListItem.module.css";
 const ListItem = (props) => {
   const string = props.escapedString;
-  return <div className={classes.user}>{string}</div>;
+  return (
+    <textarea rows={40} cols={80}>
+      {string}
+    </textarea>
+  );
 };
 
 export default ListItem;
